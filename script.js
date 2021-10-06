b56d0dbcf85c27f9c0f4a1a14506c326437445d7
KunYuChang <mischiefsub@gmail.com>
2021-10-06 15:54:23 +0800

    passing arguments to event handlers

--- script.js
+++ script.js
@@ -122,3 +122,23 @@ tabsContainer.addEventListener('click', e => {
 ///////////////////////////////////////
 
 // ⚡Menu fade animation
+const handleHover = (e, opacity) => {
+  if (e.target.classList.contains('nav__link')) {
+    const link = e.target;
+    const siblings = link.closest('.nav').querySelectorAll('.nav__link');
+    const logo = link.closest('.nav').querySelector('img');
+
+    siblings.forEach(el => {
+      if (el !== link) el.style.opacity = opacity;
+    });
+    logo.style.opacity = opacity;
+  }
+};
+
+nav.addEventListener('mouseover', e => {
+  handleHover(e, 0.5);
+});
+
+nav.addEventListener('mouseout', e => {
+  handleHover(e, 1);
+});
